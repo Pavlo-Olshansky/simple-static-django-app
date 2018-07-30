51fa183850b578c91244b7936344b30f78a765d3
pavlo_o <pavlo.olshansky@gmail.com>
2018-07-30 22:49:55 +0300

    add Service model to index app

--- pinehq/apps/index/admin.py
+++ pinehq/apps/index/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from index.models import Service
+
+admin.site.register(Service)
--- pinehq/apps/index/models.py
+++ pinehq/apps/index/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    icon = models.ImageField(
+        upload_to='service_icons/',
+        null=True,
+        blank=True,
+    )
+    title = models.CharField(
+        'Title',
+        max_length=256,
+        blank=True,
+        null=True,
+    )
+    description = models.CharField(
+        'Description',
+        max_length=2048,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        app_label = 'index'
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
